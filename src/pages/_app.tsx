b9661a4e0a107bd4d9b85c57af8f1c2ed1ae428f
0miiii <0miiii@naver.com>
2023-04-24 01:38:10 +0900

    Add redux store and global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
+import GlobalStyle from "@/styles/Global.style";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
