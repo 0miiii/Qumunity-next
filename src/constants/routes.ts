ebded3bd786fefa0a0a044340a19facb670e3e1c
0miiii <0miiii@naver.com>
2023-04-24 02:10:42 +0900

    Add route constant

---
+++ src/constants/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTE = {
+  SIGN_UP: "/sign_up",
+  SIGN_IN: "/sign_in",
+  MAIN: "/",
+  MYPAGE: "/mypage/:id",
+  DETAIL: "/detail/:id",
+  TAG_LIST: "/taglist",
+  USER_LIST: "/userlist",
+  QUESTION: "/question",
+};
