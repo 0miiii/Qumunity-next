c6a80d4e1b07d1037f78937e7b5cf10fd5336b00
0miiii <0miiii@naver.com>
2023-04-24 01:36:08 +0900

    Add store and auth slice

---
+++ src/store/reducers/authSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import {
+//   saveAccessTokenInLocalStorage,
+//   deleteAccessTokenFromLocalStorage,
+//   getAccessTokenFromLocalStorage,
+// } from "../../libs/tokenHandler";
+
+interface AuthState {
+  isLoggedIn: boolean;
+}
+
+// const token = getAccessTokenFromLocalStorage();
+
+const initialState: AuthState = {
+  isLoggedIn: false,
+  // isLoggedIn: !!token,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    // login: (state, action: PayloadAction<string>) => {
+    //   state.isLoggedIn = true;
+    //   saveAccessTokenInLocalStorage(action.payload);
+    // },
+    // logout: (state) => {
+    //   state.isLoggedIn = false;
+    //   deleteAccessTokenFromLocalStorage();
+    // },
+  },
+});
+
+// export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;
