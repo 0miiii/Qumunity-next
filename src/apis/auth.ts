1e98c815b1b4be199022e321d12b95856d63766e
0miiii <0miiii@naver.com>
2023-04-25 00:52:35 +0900

    Add auth api

---
+++ src/apis/auth.ts
@@ -0,0 +1,45 @@
+import instance from "./intance";
+
+interface IAuthResponse {
+  token: string;
+}
+
+interface IVerifyResponse {
+  _id: string;
+  nickname: string;
+  iat: number;
+  exp: number;
+}
+
+export interface ISignInUserInfo {
+  email: string;
+  password: string;
+}
+
+export interface ISignUpUserInfo extends ISignInUserInfo {
+  nickname: string;
+}
+
+const API_URL = {
+  SIGN_IN: "/auth/signin",
+  SIGN_UP: "/auth/signu",
+  VERIFY_TOKEN: "/auth/verify_token",
+};
+
+export const signInRequest = (userinfo: ISignInUserInfo) => {
+  return instance
+    .post<IAuthResponse>(API_URL.SIGN_IN, userinfo)
+    .then((res) => res.data);
+};
+
+export const signUpRequest = (userinfo: ISignUpUserInfo) => {
+  return instance
+    .post<IAuthResponse>(API_URL.SIGN_UP, userinfo)
+    .then((res) => res.data);
+};
+
+export const verifyTokenRequest = () => {
+  return instance
+    .get<IVerifyResponse>(API_URL.VERIFY_TOKEN)
+    .then((res) => res.data);
+};
