0c45502a0c5983f6315d5431b910adcf216ebe2f
mini <0miiii@naver.com>
2023-04-24 19:36:16 +0900

    Add home page

---
+++ src/pages/index.style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Top = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FilterGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
