87a45c1f79c517fd269e0469537cdac97a12fb6d
0miiii <0miiii@naver.com>
2023-04-25 01:37:05 +0900

    Add get post request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,53 @@
+import { useState } from "react";
 import Link from "next/link";
 import { useDispatch } from "react-redux";
 import { useQuery } from "react-query";
-import { verifyTokenRequest } from "@/apis";
+import { verifyTokenRequest, getPosts } from "@/apis";
 import { login } from "@/store/reducers/authSlice";
+import { ROUTE } from "@/constants";
 import Post from "@/components/Post/Post";
 import * as Styled from "@/pages/index.style";
 
 export default function Home() {
-  const { data, isSuccess } = useQuery("verify", verifyTokenRequest);
   const dispatch = useDispatch();
-  if (isSuccess) {
-    dispatch(login(data.nickname));
+  const [currentPage, setCurrentPage] = useState(1);
+  const limit = 5;
+  const { data: decodedToken, isSuccess: verifySuccess } = useQuery(
+    "verify",
+    verifyTokenRequest
+  );
+  if (verifySuccess) {
+    dispatch(login(decodedToken.nickname));
   }
+  const {
+    data: posts,
+    isLoading,
+    isError,
+  } = useQuery(["getPosts", currentPage], () => getPosts(currentPage, limit));
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
   return (
     <>
       <Styled.Top>
         <span>6 질문</span>
-        <Link href="/question">질문페이지로이동</Link>
+        <Link href={ROUTE.QUESTION}>질문페이지로이동</Link>
       </Styled.Top>
       <Styled.FilterGroup>
         <input type="text" />
         <div>정렬버튼</div>
       </Styled.FilterGroup>
-      {/* {posts?.map((post) => (
-        <Post key={post._id} post={post} />
-      ))} */}
+      <Styled.PostGroup>
+        {posts?.map((post) => (
+          <Post key={post._id} post={post} />
+        ))}
+      </Styled.PostGroup>
     </>
   );
 }
