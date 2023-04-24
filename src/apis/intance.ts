0d28b9ecfe55e381e0a60703ff454b578ba23d5a
0miiii <0miiii@naver.com>
2023-04-25 00:52:04 +0900

    Add axios intance

---
+++ src/apis/intance.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { getAccessTokenFromLocalStorage } from "@/libs/tokenHandler";
+
+const instance = axios.create({
+  baseURL: "http://localhost:3000/api",
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = getAccessTokenFromLocalStorage();
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
