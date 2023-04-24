8948e292e8ca20a499109644087048ddf43c6d41
0miiii <0miiii@naver.com>
2023-04-25 01:37:22 +0900

    Design post group

--- src/pages/index.style.ts
+++ src/pages/index.style.ts
@@ -9,3 +9,9 @@ export const FilterGroup = styled.div`
   display: flex;
   justify-content: space-between;
 `;
+
+export const PostGroup = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
