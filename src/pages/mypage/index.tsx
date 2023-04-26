9d9666de885bf12f93324ade93f458cc322ba5d1
mini <0miiii@naver.com>
2023-04-26 13:12:29 +0900

    Add tab and profile

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Profile from "@/components/Profile/Profile";
+import Tab from "@/components/Tab/Tab";
+
+const tabMenu = [
+  { title: "Questions", content: <div>컨텐츠1</div> },
+  { title: "Comments", content: <div>컨텐츠2</div> },
+  { title: "BookMarks", content: <div>컨텐츠3</div> },
+];
 
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <>
+      <div>MyPage</div>
+      <Tab tabs={tabMenu} />
+    </>
+  );
 };
 
 MyPage.auth = true;
