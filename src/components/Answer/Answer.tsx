a03ef07db0da04cba45efc2e1f259970e877a623
0miiii <0miiii@naver.com>
2023-04-28 00:39:48 +0900

    Add img tag

--- src/components/Answer/Answer.tsx
+++ src/components/Answer/Answer.tsx
@@ -10,7 +10,11 @@ const Answer: React.FC<IProps> = ({ answer }) => {
   return (
     <Styled.Container>
       <div>
-        <span>{answer.author.photoURL}</span>
+        <img
+          src={answer.author.photoURL}
+          alt={answer.author.nickname}
+          style={{ width: "20px", height: "20px" }}
+        />
         <span>{answer.author.nickname}</span>
         <span>{answer.createdAt}</span>
       </div>
