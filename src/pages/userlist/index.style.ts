1fd21bed4f0323a3cd26311f09284219de2c6783
mini <0miiii@naver.com>
2023-04-28 15:56:09 +0900

    Add user list page

---
+++ src/pages/userlist/index.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FilterGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const UserList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+`;
--- src/pages/userlist/index.tsx
+++ src/pages/userlist/index.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import Profile from "@/components/Profile/Profile";
+import { getUsers } from "@/apis";
+import * as Styled from "./index.style";
 
 const UserListPage = () => {
-  return <div>UserListPage</div>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const limit = 100;
+  const {
+    data: users,
+    isLoading,
+    isError,
+  } = useQuery("users", () => getUsers(currentPage, limit));
+
+  if (isLoading) {
+    return <div>로딩</div>;
+  }
+
+  if (isError) {
+    return <h3>에러발생</h3>;
+  }
+
+  return (
+    <>
+      <Styled.FilterGroup>
+        <input type="text" />
+        <div>
+          <button>qestion</button>
+          <button>answer</button>
+        </div>
+      </Styled.FilterGroup>
+      <Styled.UserList>
+        {users?.map((user) => (
+          <li key={user._id}>
+            <Profile user={user} />
+          </li>
+        ))}
+      </Styled.UserList>
+    </>
+  );
 };
 
 export default UserListPage;
