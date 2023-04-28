0f22730b63225d37b8432f298bc9156411f07859
mini <0miiii@naver.com>
2023-04-28 22:17:29 +0900

    Add get user post api

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,8 +1,28 @@
 import instance from "./intance";
-import { IUser } from "@/types";
+import { IUser, IPost, IAnswer } from "@/types";
+
+type TContent = "post" | "answer" | "bookmark";
 
 export const getUsers = (page: number, limit: number) => {
   return instance
     .get<IUser[]>(`/user?page=${page}&limit=${limit}`)
     .then((res) => res.data);
 };
+
+export const getUserPost = (userId: string, page: number, limit: number) => {
+  return instance
+    .get<IPost[]>(`/user/${userId}/post?page=${page}&limit=${limit}`)
+    .then((res) => res.data);
+};
+// export const getUserPost = (
+//   content: TContent,
+//   userId: string,
+//   page: number,
+//   limit: number
+// ) => {
+//   return instance
+//     .get<IPost[] | IAnswer[]>(
+//       `/user/${userId}/${content}?page=${page}&limit=${limit}`
+//     )
+//     .then((res) => res.data);
+// };
