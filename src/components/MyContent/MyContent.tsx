5290ecdf445b5eae5828acbf714fdc510c2276e4
mini <0miiii@naver.com>
2023-05-10 05:58:32 +0900

    Add my content component

--- src/components/MyContent/MyContent.tsx
+++ src/components/MyContent/MyContent.tsx
@@ -1,38 +1,30 @@
 import React, { useState } from "react";
-import { useQuery } from "react-query";
-import { useSelector } from "react-redux";
 import Pagination from "@mui/material/Pagination";
-import { RootState } from "@/store/store";
 import Post from "../Post/Post";
-import { getUserPost } from "@/apis";
+import { IAnswer, IPost } from "@/types";
 import * as Styled from "./MyContent.style";
 
-const MyContent = () => {
+interface IProps {
+  content: IPost[];
+  // content: IPost[] | IAnswer[] | undefined;
+}
+
+const MyContent: React.FC<IProps> = ({ content }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const { _id, questions } = useSelector((state: RootState) => state.auth);
-  const { data, isLoading, isError } = useQuery(
-    ["content", currentPage],
-    () => getUserPost(_id as string, currentPage, limit),
-    { enabled: !!_id }
-  );
   const limit = 4;
-  const maxPage = questions && Math.ceil(questions / limit);
+  const maxPage = content && Math.ceil(content.length / limit);
+  const startIndex = (currentPage - 1) * limit;
+  const endIndex = currentPage * limit;
+  const sliceContent = content && content.slice(startIndex, endIndex);
 
   const pageHandler = (_: React.ChangeEvent<unknown>, value: number) => {
     setCurrentPage(value);
   };
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error...</div>;
-  }
   return (
     <>
       <Styled.ListContainer>
-        {data?.map((post) => (
+        {sliceContent?.map((post) => (
           <Post key={post._id} post={post} />
         ))}
       </Styled.ListContainer>
