3de330c84d34b2ae6aa7f66b7f060e7c75df1c97
mini <0miiii@naver.com>
2023-05-10 03:38:36 +0900

    Refactor to assign string by removing destructuring assignment

--- src/apis/intance.ts
+++ src/apis/intance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getAuthInfoFromLocalStorage } from "@/libs/tokenHandler";
+import { getAccessTokenFromLocalStorage } from "@/libs/tokenHandler";
 
 const instance = axios.create({
   baseURL: "/api",
@@ -7,7 +7,7 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   (config) => {
-    const { token } = getAuthInfoFromLocalStorage();
+    const token = getAccessTokenFromLocalStorage();
     if (token) {
       config.headers.Authorization = token;
     }
