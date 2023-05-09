ccfd4a84aa76254b05ffac102a4222add9834839
mini <0miiii@naver.com>
2023-05-10 06:05:22 +0900

    Fix a route when refreshing, but there is a bug that causes an error

--- src/components/Layouts/AuthGuardLayout.tsx
+++ src/components/Layouts/AuthGuardLayout.tsx
@@ -9,21 +9,11 @@ interface Iprops {
 }
 
 export const AuthGaurdLayout: React.FC<Iprops> = ({ children }) => {
-  const [loading, setLoading] = useState(true);
   const { isLoggedIn } = useSelector((state: RootState) => state.auth);
   const route = useRouter();
 
-  useEffect(() => {
-    if (!isLoggedIn) {
-      alert("로그인이 필요합니다");
-      route.push(ROUTE.SIGN_IN);
-    } else {
-      setLoading(false);
-    }
-  }, [isLoggedIn, route]);
-
-  if (loading) {
-    return <div>loading...</div>;
+  if (!isLoggedIn) {
+    route.push(ROUTE.SIGN_IN);
   }
 
   return <>{children}</>;
