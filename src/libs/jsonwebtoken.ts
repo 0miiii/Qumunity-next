3b6db46bc3696349fed08d9ec1c66f65b07ac2db
mini <0miiii@naver.com>
2023-05-10 03:44:06 +0900

    Update the expiration period to 10 minutes and add the photoURL to the payload

--- src/libs/jsonwebtoken.ts
+++ src/libs/jsonwebtoken.ts
@@ -3,11 +3,12 @@ import jwt from "jsonwebtoken";
 interface Ipayload {
   _id: string;
   nickname: string;
+  photoURL: string;
 }
 
 export const createToken = (payload: Ipayload) => {
   return jwt.sign(payload, process.env.NEXT_PUBLIC_JWT_SECRET_KEY as string, {
-    expiresIn: "30m",
+    expiresIn: "10m",
   });
 };
 
