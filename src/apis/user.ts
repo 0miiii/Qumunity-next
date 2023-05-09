c700b9f93755a6298dc14618e1a2f213e8fca64c
mini <0miiii@naver.com>
2023-05-10 05:58:07 +0900

    Add get user info api

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,7 +1,11 @@
 import instance from "./intance";
 import { IUser, IPost, IAnswer } from "@/types";
 
-type TContent = "post" | "answer" | "bookmark";
+interface IGetUserInfo {
+  userInfo: IUser;
+  userPosts: IPost[];
+  userAnswers: IAnswer[];
+}
 
 export const getUsers = (page: number, limit: number) => {
   return instance
@@ -14,15 +18,7 @@ export const getUserPost = (userId: string, page: number, limit: number) => {
     .get<IPost[]>(`/user/${userId}/post?page=${page}&limit=${limit}`)
     .then((res) => res.data);
 };
-// export const getUserPost = (
-//   content: TContent,
-//   userId: string,
-//   page: number,
-//   limit: number
-// ) => {
-//   return instance
-//     .get<IPost[] | IAnswer[]>(
-//       `/user/${userId}/${content}?page=${page}&limit=${limit}`
-//     )
-//     .then((res) => res.data);
-// };
+
+export const getUserInfo = (userId: string) => {
+  return instance.get<IGetUserInfo>(`/user/${userId}`).then((res) => res.data);
+};
